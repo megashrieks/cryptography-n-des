20ef9fe28193b2d4e42c1ecaf9580933fb276573
megashrieks <shrikanthbudya@gmail.com>
2019-03-30 18:44:55 +0530

    minor changes

--- input/keys.js
+++ input/keys.js
@@ -3,4 +3,12 @@
 //deciding the "n" in n-des
 //so des will be conducted n times on the given message
 
-module.exports = ["abcdef", "ghijkl", "mnopqr", "stuvwx", "yzabcd"];
+module.exports = [
+	//genuine 64 bit key in hexadecimal
+	"133457799BBCDFF1",
+	"abcdef",
+	"ghijkl",
+	"mnopqr",
+	"stuvwx",
+	"yzabcd"
+];
